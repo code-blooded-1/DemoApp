8586d4d6b180471133a4015a5c9bbc0d81162528
Mayank <mswebsite01@gmail.com>
2023-02-15 14:04:55 +0530

    Added login and logout session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(name: params[:session][:name])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to user
+        else
+            flash[:alert] = "There was something wrong with your login details"
+            redirect_to '/login'
+        end
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged out"
+        redirect_to users_path
     end
 end
